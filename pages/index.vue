50b5c709b44ce919acb8c39f21856d56f619f92d
caboe <ca_boe@yahoo.de>
2021-03-21 19:57:49 +0100

    add refs array

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        test-refs
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="hello">
+   <div v-for="(item, idx) in list" :ref="setItemRef" :key="idx">foo {{ idx }}</div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import { onBeforeUpdate, onUpdated, onMounted } from '@nuxtjs/composition-api'
 
-export default Vue.extend({})
+export default {
+  setup() {
+    let itemRefs = []
+    let list = [1,2,3]
+    const setItemRef = el => {
+      console.log(el);
+      if (el) {
+        itemRefs.push(el)
+      }
+    }
+    onBeforeUpdate(() => {
+      itemRefs = []
+    })
+    onUpdated(() => {
+      console.log(itemRefs)
+    })
+    onMounted(() => {
+      console.log('itemRefs', itemRefs);
+    })
+    return {
+      setItemRef,
+      list
+    }
+  }
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
